cfef1af20ed555da8256b0eeffd293c1e44a4506
Sue Sue <29suesue@gmail.com>
2024-10-05 16:40:03 -0700

    feat: add reset to 0

--- src/api/admin.py
+++ src/api/admin.py
@@ -17,7 +17,19 @@ def reset():
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
     with db.engine.begin() as connection:
-        connection.execute(sqlalchemy.text(
-            "UPDATE gobal_inventory SET num_green_potions = 0, num_green_ml = 0, gold = 100 WHERE id = 1" ))
-    return "OK"
+    connection.execute(sqlalchemy.text("""
+        UPDATE global_inventory 
+        SET num_green_potions = 0, 
+            num_green_ml = 0, 
+            num_red_potions = 0, 
+            num_red_ml = 0, 
+            num_blue_potions = 0, 
+            num_blue_ml = 0, 
+            num_dark_potions = 0, 
+            num_dark_ml = 0, 
+            gold = 100 
+        WHERE id = 1
+    """))
+
+    return {"message": "Shop has been reset to 0."}
 
