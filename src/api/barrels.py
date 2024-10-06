77ab7755f631a39313a6dbafb3154791e60989ce
Sue Sue <29suesue@gmail.com>
2024-10-05 18:19:39 -0700

    feat: add barrels purchase options for different potions.

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,56 +26,105 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     # Fetch the data first to ensure we are not overwriting the existing values.
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(
-            "SELECT num_green_potions, num_green_ml, gold FROM global_inventory WHERE id = 1"
+            "SELECT num_red_ml, num_green_ml, num_blue_ml, num_dark_ml, gold FROM global_inventory"
         ))
         
         row = result.fetchone()
         if row:
-            cur_num_green_potions = row[0]
+            cur_num_red_ml = row[0]
             cur_num_green_ml = row[1]
-            cur_gold = row[2]
+            cur_num_blue_ml = row[2]
+            cur_num_dark_ml = row[3]
+            cur_gold = row[4]
 
+        # Update the ml (inventory) after the barrels are delivered.
         for barrel in barrels_delivered:
-            if barrel.sku.upper() == "SMALL_GREEN_BARREL":
+            if "RED" barrel.sku.upper():
+                cur_num_red_ml += barrel.ml_per_barrel * barrel.quantity
+            elif "GREEN" barrel.sku.upper():
                 cur_num_green_ml += barrel.ml_per_barrel * barrel.quantity
-                cur_gold -= barrel.price * barrel.quantity
+            elif "BLUE" barrel.sku.upper():
+                cur_num_blue_ml += barrel.ml_per_barrel * barrel.quantity
+            elif "DARK" barrel.sku.upper():
+                cur_num_dark_ml += barrel.ml_per_barrel * barrel.quantity
 
+            # Subtract gold for the delivered barrels
+            cur_gold -= barrel.price * barrel.quantity
+        
+        # Then we update the GI table with the new values
         connection.execute(sqlalchemy.text(
-            "UPDATE global_inventory SET num_green_ml=:num_green_ml, gold=:gold WHERE id = 1"
+            """UPDATE global_inventory
+                SET num_red_ml=: num_red_ml,
+                num_green_ml=:num_green_ml,
+                num_blue_ml=:num_blue_ml,
+                num_dark_ml=:num_dark_ml,
+                gold=:gold
+                """
         ), {
+            'num_red_ml' : cur_num_red_ml,
             'num_green_ml' : cur_num_green_ml,
+            'num_blue_ml' : cur_num_blue_ml,
+            'num_dark_ml' : cur_num_dark_ml,
             'gold': cur_gold
         })
 
-    print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
+    print(f"barrels delivered: {barrels_delivered} order_id: {order_id}")
 
-    return "OK"
+    return {"message": "Delivered barrels and added ml to inventory of all potions."}
 
 # Gets called once a day
 @router.post("/plan")
 def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    """ Purchase a new barrel if the potion inventory is low. """
+    # helps decide when and what potion barrels to purchase from a supplier 
+    # (wholesaler) based on your current inventory and available gold.
+    """ Purchase a new barrel for r,g,b,d if the potion inventory is low. """
     print(wholesale_catalog)
     
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(
-            "SELECT num_green_potions, num_green_ml, gold FROM global_inventory WHERE id = 1"
+            "SELECT num_red_potions, num_green_potions, num_blue_potions, num_dark_potions, gold FROM global_inventory WHERE id = 1"
         ))
     
     row = result.fetchone()
     if row:
-        cur_num_green_potions = row[0]
-        cur_num_green_ml = row[1]
-        cur_gold = row[2]
+        cur_num_red_potions = row[0]
+        cur_num_green_potions = row[1]
+        cur_num_blue_potions = row[2]
+        cur_num_dark_potions = row[3]
+        cur_gold = row[4]
 
-    # Purchase a new small green potion barrel only if the number of potions in inventory is less than 10
+    purchase_plan = []
+
+    # Purchase logic for different sizes
     for barrel in wholesale_catalog:
-        if barrel.sku.upper() == "SMALL_GREEN_BARREL" and cur_num_green_potions < 10 and cur_gold >= barrel.price:
-            return [
-            {
-                "sku": "SMALL_GREEN_BARREL",
-                "quantity": 1,
-            }
-        ]
-    return [] # Retrun an empty plan if purchase is not needed
+        if "RED" in barrel.sku.upper():
+            if barrel.sku.upper() == "SMALL_RED_BARREL" and cur_num_red_potions < 10 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "SMALL_RED_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MEDIUM_RED_BARREL" and cur_num_red_potions < 20 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MEDIUM_RED_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MINI_RED_BARREL" and cur_num_red_potions < 5 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MINI_RED_BARREL", "quantity: 1"})
+        elif "GREEN" in barrel.sku.upper():
+            if barrel.sku.upper() == "SMALL_GREEN_BARREL" and cur_num_green_potions < 10 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "SMALL_GREEN_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MEDIUM_GREEN_BARREL" and cur_num_green_potions < 20 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MEDIUM_GREEN_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MINI_GREEN_BARREL" and cur_num_green_potions < 5 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MINI_GREEN_BARREL", "quantity: 1"})
+        elif "BLUE" in barrel.sku.upper():
+            if barrel.sku.upper() == "SMALL_BLUE_BARREL" and cur_num_blue_potions < 10 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "SMALL_BLUE_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MEDIUM_BLUE_BARREL" and cur_num_blue_potions < 20 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MEDIUM_BLUE_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MINI_BLUE_BARREL" and cur_num_blue_potions < 5 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MINI_BLUE_BARREL", "quantity: 1"})
+        elif "DARK" in barrel.sku.upper():
+            if barrel.sku.upper() == "SMALL_DARK_BARREL" and cur_num_dark_potions < 10 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "SMALL_DARK_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MEDIUM_DARK_BARREL" and cur_num_dark_potions < 20 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MEDIUM_DARK_BARREL", "quantity: 1"})
+            elif barrel.sku.upper() == "MINI_DARK_BARREL" and cur_num_dark_potions < 5 and cur_gold >= barrel.price:
+                purchase_plan.append({"sku": "MINI_DARK_BARREL", "quantity: 1"})
+
+    return purchase_plan if purchase_plan else [] # Return an empty plan if purchase is not needed
 
